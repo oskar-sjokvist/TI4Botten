43ef1c0d9213d54a2ff2d80296cf546994ed2bd2
fsharpasharp <jnliuu@gmail.com>
2025-08-12 09:43:53 +0200

    Reorder

--- src/game/controller.py
+++ src/game/controller.py
@@ -1,6 +1,7 @@
-from . import model
 from sqlalchemy.orm import Session
 from typing import List
+
+from . import model
 
 def players_ordered_by_turn(session : Session, game : model.Game) -> List[model.GamePlayer]:
     return session.query(model.GamePlayer).with_parent(game).order_by(model.GamePlayer.turn_order.asc()).all()
